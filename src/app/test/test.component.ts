87b4a77cace429de44c9d713a580c5bf7046ed4d
Chaitanya GaneshRaju Sarikonda <csarikonda@uworld.in>
2023-09-09 18:31:36 +0530

    Session-1 initial commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+
+export class TestComponent {
+  constructor() { }
+
+  name: string = "chaitanya Ganesh";
+
+  ngModelVar: string = "text";
+
+  disableButton: boolean = false;
+
+  redOrBlue: boolean = false;
+
+  logText(){
+    console.log("Button has been clicked")
+  }
+
+}
